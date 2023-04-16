fa9240a625741186a1a7b8d8e64d5c53e2075506
Pshenichka1 <pshenichka.olga@gmail.com>
2023-04-16 22:11:54 +0300

    pagination

--- src/js-partials/handle-search-form.js
+++ src/js-partials/handle-search-form.js
@@ -2,6 +2,7 @@ import Notiflix, { Notify } from 'notiflix';
 import { UnsplashAPI } from "./api.js";
 import {getGenres } from './api';
 import { createGalleryMarkup } from './create-gallery-markup';
+import { createPagination } from './pagination';
 
 const searchFormEl = document.querySelector('.movie-search');
 const galleryListEl = document.querySelector('.gallery__container');
@@ -28,7 +29,18 @@ const handleSearchFormSubmit = async event => {
         // const formattedData = dataFormat(data.results, ganres);
         const ganres = await getGenres();
         galleryListEl.innerHTML = createGalleryMarkup(data.results, ganres);
-        
+
+        const pagination = createPagination(data.total_results, data.total_pages);
+      pagination.on('beforeMove', ({ page }) => {
+        // showHideLoader(loader);
+        refs.gallery.innerHTML = '';
+        getByKeyword(query, page).then(data => {
+        //   showHideLoader(loader);
+          refs.gallery.innerHTML = createGalleryMarkup(data.results);
+        //   scrollOnTop();
+        });
+      });
+         
     } catch (err) {
         console.log(err);
     }
---
+++ src/js-partials/pagination.js
@@ -0,0 +1,24 @@
+import Pagination from 'tui-pagination';
+import 'tui-pagination/dist/tui-pagination.css';
+
+import { getNameFilm, getTrending } from './api'
+const TUI_VISIBLE_PAGES = 5;
+const paginationEl = document.querySelector('#pagination');
+
+export function createPagination(totalItems, visiblePages) {
+  const options = {
+    itemsPerPage: 20,
+    totalItems: totalItems,
+    visiblePages: visiblePages < 5 ? visiblePages : TUI_VISIBLE_PAGES,
+  };
+
+  const pagination = new Pagination(paginationEl, options);
+
+  if (visiblePages > 1) {
+    paginationEl.style.display = 'block';
+  } else {
+    paginationEl.style.display = 'none';
+  }
+
+  return pagination;
+}
