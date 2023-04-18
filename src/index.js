6d4ddccb0ca86bdda729f1027e8de69d5dc8da94
Julia_Ivanova <109332548+Julia27Ivanova@users.noreply.github.com>
2023-04-18 17:43:43 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import { getTrending } from './js-partials/api';
 import './js-partials/loader';
 import * as renderingMovieCard from './js-partials/rendering-movie-cards';
+import './js-partials/local';
+import './js-partials/watched';
+import './js-partials/queue';
+import './js-partials/btn-local';
