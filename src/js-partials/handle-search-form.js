bc6d55645fa6066b0b05d16eff78699b322e8600
ViktorZinkevych <115555643+ViktorZinkevych@users.noreply.github.com>
2023-04-18 12:18:29 +0200

    add pagination
    добавил пагинацию и скролл на верх

--- src/js-partials/handle-search-form.js
+++ src/js-partials/handle-search-form.js
@@ -3,7 +3,7 @@ import { UnsplashAPI } from "./api.js";
 import {getGenres } from './api';
 import { createGalleryMarkup } from './create-gallery-markup';
 import { createPagination } from './pagination';
-
+import { scrollOnTop } from './scrollOnTop.js';
 const searchFormEl = document.querySelector('.movie-search');
 const galleryListEl = document.querySelector('.gallery__container');
 const inputEl = document.querySelector('.movie-search-text');
@@ -15,30 +15,37 @@ const unsplashApi = new UnsplashAPI();
 const handleSearchFormSubmit = async event => {
     event.preventDefault();
 
-    const searchQuery = inputEl.value.trim();
-    unsplashApi.q = searchQuery;
-
+    const query = inputEl.value.trim();
+    unsplashApi.q = query;
+   
+   
     try{
         const { data } = await unsplashApi.fetchMovies();
-       
+        
         if(data.total_results === 0) {
             Notify.failure("Sorry, there are no movies matching your search query. Please try again.");
             return;
         }
         Notify.success(`Hooray! We found ${data.total_results} movies.`)
-        // const formattedData = dataFormat(data.results, ganres);
+       
         const ganres = await getGenres();
+        
         galleryListEl.innerHTML = createGalleryMarkup(data.results, ganres);
 
         const pagination = createPagination(data.total_results, data.total_pages);
-      pagination.on('beforeMove', ({ page }) => {
-        // showHideLoader(loader);
-        refs.gallery.innerHTML = '';
-        getByKeyword(query, page).then(data => {
-        //   showHideLoader(loader);
-          refs.gallery.innerHTML = createGalleryMarkup(data.results);
-        //   scrollOnTop();
-        });
+      pagination.on('afterMove', async ({ page }) => {
+
+        unsplashApi.page = page;
+        
+        galleryListEl.innerHTML = '';
+            //   showHideLoader(loader);
+        
+        const { data } = await unsplashApi.fetchMovies(page);
+      
+        galleryListEl.innerHTML = createGalleryMarkup(data.results, ganres);
+        
+        scrollOnTop();
+        
       });
          
     } catch (err) {
