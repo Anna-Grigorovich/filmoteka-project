7260b7fdff930c581a07f5f75a90fdd58d47f2cb
ViktorZinkevych <115555643+ViktorZinkevych@users.noreply.github.com>
2023-04-18 21:40:15 +0200

    loader
    подключил при загрузке популярных фильмов, фильмов по сабмиту в инпуте и там и там к пагинации. Вроде появляется кружок но очень быстро исчезает))))

--- src/js-partials/handle-search-form.js
+++ src/js-partials/handle-search-form.js
@@ -4,10 +4,14 @@ import {getGenres } from './api';
 import { createGalleryMarkup } from './create-gallery-markup';
 import { createPagination } from './pagination';
 import { scrollOnTop } from './scrollOnTop.js';
+
+import { showHideLoader } from './loader.js';
+
 const searchFormEl = document.querySelector('.movie-search');
 const galleryListEl = document.querySelector('.gallery__container');
 const inputEl = document.querySelector('.movie-search-text');
 
+const loader = document.querySelector('.loader');
 
 
 const unsplashApi = new UnsplashAPI();
@@ -20,9 +24,11 @@ const handleSearchFormSubmit = async event => {
    
    
     try{
+      showHideLoader(loader);
         const { data } = await unsplashApi.fetchMovies();
         
         if(data.total_results === 0) {
+          showHideLoader(loader); 
             Notify.failure("Sorry, there are no movies matching your search query. Please try again.");
             return;
         }
@@ -31,19 +37,19 @@ const handleSearchFormSubmit = async event => {
         const ganres = await getGenres();
         
         galleryListEl.innerHTML = createGalleryMarkup(data.results, ganres);
-
+        showHideLoader(loader);
         const pagination = createPagination(data.total_results, data.total_pages);
       pagination.on('afterMove', async ({ page }) => {
 
         unsplashApi.page = page;
         
         galleryListEl.innerHTML = '';
-            //   showHideLoader(loader);
+        showHideLoader(loader); 
         
         const { data } = await unsplashApi.fetchMovies(page);
       
         galleryListEl.innerHTML = createGalleryMarkup(data.results, ganres);
-        
+        showHideLoader(loader);
         scrollOnTop();
         
       });
